32030bd1ecacf3555476773b9b8d9b3d9a6d832d
Anderson da Mata <anderson.daamata@gmail.com>
2022-06-07 00:11:41 -0300

    login page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto - BI</title>
+    <link rel="stylesheet" href="{{ URL('public/assets/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{ URL('public/assets/css/style.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    
+</head>
+<body>
+<div class="limiter" id="login">
+        <div class="container_login" style="
+        background-image:url(https://img.freepik.com/fotos-gratis/corretor-masculino-usando-estatisticas-de-troca-de-forex-em-varios-monitores-trabalhando-com-graficos-de-mercado-e-vendas-de-acoes-em-tempo-real-participacao-nos-lucros-do-fundo-de-hedge-para-comprar-e-vender-dinheiro-tiro-de-tripe_482257-37652.jpg?t=st=1652723110~exp=1652723710~hmac=34022ef0549dee94fd58531206655037f8032f40ad0b67b0603c60e7ec56853a&w=900)">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6"></div>
+                    <div class="col-md-5 col-md-offset-1">
+                        <div class="login_topimg" style="
+                        background: #91b3d1 url(https://image.freepik.com/free-vector/cyber-data-security-online-concept-illustration-internet-security-information-privacy-protection_1150-37330.jpg) no-repeat;
+                        background-size: auto;
+                        background-position: center;
+                        ">
+                        </div>
+                        <div class="login_container_formulario">
+                            <form class="" action="" method="POST">
+                                <h5 class="text-center text-uppercase"> Login </h5> 
+                                <p class="text-center">Insira suas credenciais</p>
+                                <div class="input_login my-3">
+                                    <input class="form-control" type="email" name="user" placeholder="Usuário">
+                                </div>
+                                <div class="input_login my-3">
+                                    <input class="form-control" type="pass" name="pass" placeholder="Senha">
+                                </div>
+                                <div class="flex-sb-m w-full p-b-30">
+                                    <div class="contact100-form-checkbox">
+                                        <input class="input_checkbox" id="ckb1" type="checkbox" name="remember-me">
+                                        <label class="label_checkbox" for="ckb1">
+                                            Lembrar-me
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <a href="#" class="txt1">
+                                            Esqueceu sua senha?
+                                        </a>
+                                    </div>
+                                </div>
+                                <button class="btn botao_verde w-100 text-uppercase"> Entrar </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript" href="{{ URL('public/assets/js/jquery-3.6.0.min.js')}}">
+</body>
+</html>
